9d1ef7a6f06197f4e0598e881d793d525065c776
Berlin Ortega <marlitros200002@gmail.com>
2025-06-24 00:31:23 -0600

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,9 @@
 import server from './server';
 import colors from 'colors';
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 4000;
 
-server.listen(port, () => {
-    console.log(colors.cyan.bold(`Server is running on http://localhost:${port}`));
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port ${port}`);
 });
+
