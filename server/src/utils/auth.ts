ba2e1410a9c881c2e4d086e50e763e6b6d3adb45
Berlin Ortega <marlitros200002@gmail.com>
2025-06-25 15:15:47 -0600

    bcryptjs

--- server/src/utils/auth.ts
+++ server/src/utils/auth.ts
@@ -1,9 +1,9 @@
-import bcrypt from 'bcrypt'
+import bcryptjs from 'bcryptjs'
 
 export const hashPassword = async (password: string) => {
-    const salt = await bcrypt.genSalt(10)
-    return await bcrypt.hash(password, salt)
+    const salt = await bcryptjs.genSalt(10)
+    return await bcryptjs.hash(password, salt)
 }
 export const comparePasswords = async (password : string, hashedPassword : string) => {
-    return await bcrypt.compare(password, hashedPassword)
+    return await bcryptjs.compare(password, hashedPassword)
 }
